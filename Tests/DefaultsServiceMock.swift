feb0c756d5ffa13fe2fba1117b424f540b7c6fa7
mttcrsp <crespi.matteo@icloud.com>
2020-02-11 23:02:45 +0100

    Add defaults service mock

---
+++ Tests/DefaultsServiceMock.swift
@@ -0,0 +1,14 @@
+@testable
+import Fosdem
+
+final class DefaultsServiceMock: DefaultsService {
+    private(set) var results: [String: Any] = [:]
+
+    override func value<Value>(for key: Key<Value>) -> Value? {
+        return results[key.name] as? Value
+    }
+
+    override func set<Value>(_ value: Value?, for key: Key<Value>) {
+        results[key.name] = value
+    }
+}
