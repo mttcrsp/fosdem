d62537d2da2f8360bc65986f62eea8cfaa5d09ff
mttcrsp <crespi.matteo@icloud.com>
2020-02-11 23:08:15 +0100

    Add events view controller

---
+++ Sources/ViewControllers/EventsViewController.swift
@@ -0,0 +1,78 @@
+import UIKit
+
+protocol EventsViewControllerDataSource: AnyObject {
+    var events: [Event] { get }
+}
+
+protocol EventsViewControllerDelegate: AnyObject {
+    func eventsViewController(_ eventsViewController: EventsViewController, didSelect event: Event)
+}
+
+final class EventsViewController: UITableViewController {
+    weak var dataSource: EventsViewControllerDataSource?
+    weak var delegate: EventsViewControllerDelegate?
+
+    private var events: [Event] {
+        dataSource?.events ?? []
+    }
+
+    init() {
+        if #available(iOS 13.0, *) {
+            super.init(style: .insetGrouped)
+        } else {
+            super.init(style: .grouped)
+        }
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.reuseIdentifier)
+    }
+
+    override func numberOfSections(in _: UITableView) -> Int {
+        events.count
+    }
+
+    override func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.reuseIdentifier, for: indexPath)
+        cell.configure(with: event(for: indexPath.section))
+        return cell
+    }
+
+    override func tableView(_: UITableView, titleForHeaderInSection section: Int) -> String? {
+        DateComponentsFormatter.default.string(from: event(for: section).start)
+    }
+
+    override func tableView(_: UITableView, willDisplayHeaderView view: UIView, forSection _: Int) {
+        guard let view = view as? UITableViewHeaderFooterView else { return }
+        view.textLabel?.font = .preferredFont(forTextStyle: .subheadline)
+    }
+
+    private func event(for section: Int) -> Event {
+        events[section]
+    }
+}
+
+private extension DateComponentsFormatter {
+    static let `default`: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute]
+        return formatter
+    }()
+}
+
+private extension UITableViewCell {
+    func configure(with event: Event) {
+        textLabel?.text = event.title
+        textLabel?.numberOfLines = 0
+        accessoryType = .disclosureIndicator
+    }
+}
