408f2196954909ffe2fec995f922dc11002c1283
mttcrsp <crespi.matteo@icloud.com>
2020-02-16 23:25:37 +0100

    Add Person GRDB table info

---
+++ Sources/Model/Person+GRDB.swift
@@ -0,0 +1,20 @@
+import GRDB
+
+extension Person: PersistableRecord, FetchableRecord {
+    static var databaseTableName: String {
+        "people"
+    }
+
+    enum Columns: String, ColumnExpression {
+        case id, name
+    }
+
+    init(row: Row) {
+        self.init(id: row[Columns.id], name: row[Columns.name])
+    }
+
+    func encode(to container: inout PersistenceContainer) {
+        container[Columns.id.rawValue] = id
+        container[Columns.name.rawValue] = name
+    }
+}
