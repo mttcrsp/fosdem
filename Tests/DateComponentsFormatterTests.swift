e06e1f5584f92c5835ce717cfcf1102fb0a8fd82
mttcrsp <crespi.matteo@icloud.com>
2020-02-18 02:16:32 +0100

    Add date components formatting tests

---
+++ Tests/DateComponentsFormatterTests.swift
@@ -0,0 +1,11 @@
+@testable
+import Fosdem
+import XCTest
+
+final class DateComponentsFormatterTests: XCTestCase {
+    func testTime() {
+        let formatter = DateComponentsFormatter.time
+        XCTAssertEqual(formatter.string(from: .init(hour: 9, minute: 0)), "9:00")
+        XCTAssertEqual(formatter.string(from: .init(hour: 23, minute: 59)), "23:59")
+    }
+}
