f5a4de5d59ca0427244ff6f0be528ab539eade40
mttcrsp <crespi.matteo@icloud.com>
2020-02-26 23:37:57 +0100

    Add window subclass with proxies customizations

---
+++ Sources/Views/Window.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+final class Window: UIWindow {
+    func configure() {
+        tintColor = .fos_label
+        backgroundColor = .fos_systemBackground
+    }
+
+    func configureAppearanceProxies() {
+        let navigationBar = UINavigationBar.appearance()
+        navigationBar.shadowImage = UIImage()
+        navigationBar.isTranslucent = false
+
+        let tabBar = UITabBar.appearance()
+        tabBar.shadowImage = UIImage()
+        tabBar.isTranslucent = false
+    }
+}
