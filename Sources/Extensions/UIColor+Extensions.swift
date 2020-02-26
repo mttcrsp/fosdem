df5fbbf40dc3ced8fc5f784ab9ae8f39d2a7e90e
mttcrsp <crespi.matteo@icloud.com>
2020-02-26 23:37:19 +0100

    Add some UIColor backward compatibility extensions

--- Sources/Extensions/UIColor+Extensions.swift
+++ Sources/Extensions/UIColor+Extensions.swift
@@ -1,6 +1,24 @@
 import UIKit
 
 extension UIColor {
+    static var fos_systemBackground: UIColor {
+        if #available(iOS 13.0, *) {
+            return .systemBackground
+        } else {
+            return .white
+        }
+    }
+
+    static var fos_label: UIColor {
+        if #available(iOS 13.0, *) {
+            return .label
+        } else {
+            return .black
+        }
+    }
+}
+
+extension UIColor {
     func adjustingBrightness(byFactor factor: CGFloat) -> UIColor {
         var hue = CGFloat(0), saturation = CGFloat(0), brightness = CGFloat(0), alpha = CGFloat(0)
         getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
