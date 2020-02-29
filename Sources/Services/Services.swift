242a2aa469de3ffe7af89aabf797777f640a2e1f
mttcrsp <crespi.matteo@icloud.com>
2020-02-29 22:39:40 +0100

    Wire up activity service

--- Sources/Services/Services.swift
+++ Sources/Services/Services.swift
@@ -4,6 +4,7 @@ final class Services {
     let acknowledgementsService: AcknowledgementsService
     let persistenceService: PersistenceService
     let favoritesService: FavoritesService
+    let activityService: ActivityService
 
     init() throws {
         let applicationSupportURL = try FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
@@ -11,5 +12,6 @@ final class Services {
         persistenceService = try PersistenceService(path: databaseURL.path)
         acknowledgementsService = AcknowledgementsService()
         favoritesService = FavoritesService()
+        activityService = ActivityService()
     }
 }
