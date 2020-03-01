1db7f68e0c0f6644ae389fb47b078d33522fedc1
mttcrsp <crespi.matteo@icloud.com>
2020-03-01 02:36:06 +0100

    Extra default date formatter

--- Sources/Extensions/XMLDecoder+Extensions.swift
+++ Sources/Extensions/XMLDecoder+Extensions.swift
@@ -3,12 +3,17 @@ import XMLCoder
 
 extension XMLDecoder {
     static let `default`: XMLDecoder = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-
         let decoder = XMLDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
-        decoder.dateDecodingStrategy = .formatted(formatter)
+        decoder.dateDecodingStrategy = .formatted(.default)
         return decoder
+    }()
+}
+
+extension DateFormatter {
+    static let `default`: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
     }()
 }
