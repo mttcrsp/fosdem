27f40d49c99ae57a1572aea87a506eac330bdb1f
mttcrsp <crespi.matteo@icloud.com>
2020-03-14 11:21:04 +0100

    Add persistence service write tests

--- Tests/PersistenceServiceTests.swift
+++ Tests/PersistenceServiceTests.swift
@@ -3,4 +3,43 @@ import Fosdem
 import GRDB
 import XCTest
 
-final class PersistenceServiceTests: XCTestCase {}
+final class PersistenceServiceTests: XCTestCase {
+    func testWrite() {
+        XCTAssertNoThrow(({
+            let service = try PersistenceService(path: nil, migrations: [])
+            let expectation = self.expectation(description: #function)
+            let operation = Write { _ in }
+
+            service.performWrite(operation) { error in
+                expectation.fulfill()
+                XCTAssertNil(error)
+            }
+
+            self.waitForExpectations(timeout: 0.1)
+        }))
+    }
+
+    func testWriteError() {
+        XCTAssertNoThrow(({
+            let service = try PersistenceService(path: nil, migrations: [])
+            let expectation = self.expectation(description: #function)
+            let error = NSError(domain: "org.fosdem.fosdem", code: -1)
+            let operation = Write { _ in throw error }
+
+            service.performWrite(operation) { receivedError in
+                expectation.fulfill()
+                XCTAssertEqual(receivedError as NSError?, error)
+            }
+
+            self.waitForExpectations(timeout: 0.1)
+        }))
+    }
+
+    private struct Write: PersistenceServiceWrite {
+        let perform: (Database) throws -> Void
+
+        func perform(in database: Database) throws {
+            try perform(database)
+        }
+    }
+}
