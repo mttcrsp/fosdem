47948b3cfe9dd56b4afe46de091ea2919a3787f6
mttcrsp <crespi.matteo@icloud.com>
2020-03-14 14:44:08 +0100

    Add persistence service read tests

--- Tests/PersistenceServiceTests.swift
+++ Tests/PersistenceServiceTests.swift
@@ -23,7 +23,8 @@ final class PersistenceServiceTests: XCTestCase {
         XCTAssertNoThrow(try {
             let service = try PersistenceService(path: nil, migrations: [])
             let expectation = self.expectation(description: #function)
-            let error = NSError(domain: "org.fosdem.fosdem", code: -1)
+
+            let error = makeError()
             let operation = Write { _ in throw error }
 
             service.performWrite(operation) { receivedError in
@@ -35,10 +36,66 @@ final class PersistenceServiceTests: XCTestCase {
         }())
     }
 
+    func testRead() {
+        XCTAssertNoThrow(try {
+            let service = try PersistenceService(path: nil, migrations: [])
+            let expectation = self.expectation(description: #function)
+            let operation = Read { _ in 99 }
+
+            service.performRead(operation) { result in
+                expectation.fulfill()
+
+                switch result {
+                case let .success(value):
+                    XCTAssertEqual(value, 99)
+                case let .failure(error):
+                    XCTFail(error.localizedDescription)
+                }
+            }
+
+            self.waitForExpectations(timeout: 0.1)
+        }())
+    }
+
+    func testReadError() {
+        XCTAssertNoThrow(try {
+            let service = try PersistenceService(path: nil, migrations: [])
+            let expectation = self.expectation(description: #function)
+
+            let error = makeError()
+            let operation = Read { _ in throw error }
+
+            service.performRead(operation) { result in
+                expectation.fulfill()
+
+                switch result {
+                case let .success(value):
+                    XCTFail("Unexpected operation success with value '\(value)'")
+                case let .failure(receivedError):
+                    XCTAssertEqual(receivedError as NSError, error)
+                }
+            }
+
+            self.waitForExpectations(timeout: 0.1)
+        }())
+    }
+
+    private func makeError() -> NSError {
+        .init(domain: "org.fosdem.fosdem", code: -1)
+    }
+
     private struct Write: PersistenceServiceWrite {
         let perform: (Database) throws -> Void
 
         func perform(in database: Database) throws {
+            try perform(database)
+        }
+    }
+
+    private struct Read: PersistenceServiceRead {
+        let perform: (Database) throws -> Int
+
+        func perform(in database: Database) throws -> Int {
             try perform(database)
         }
     }
