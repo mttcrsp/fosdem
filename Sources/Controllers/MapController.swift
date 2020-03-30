278fbd6943068903d49b2ce1401254646610cd50
mttcrsp <crespi.matteo@icloud.com>
2020-03-30 21:32:20 +0200

    Basic integration of blueprints screen

--- Sources/Controllers/MapController.swift
+++ Sources/Controllers/MapController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 final class MapController: UIViewController {
     private weak var mapViewController: MapViewController?
+    private weak var blueprintsViewController: BlueprintsViewController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,18 +16,82 @@ final class MapController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         mapViewController?.view.frame = view.bounds
+
+        guard let blueprintsView = blueprintsViewController?.view else { return }
+
+        blueprintsView.frame.size.width = view.bounds.width - view.layoutMargins.left - view.layoutMargins.right
+        blueprintsView.frame.size.height = 200
+        blueprintsView.frame.origin.x = view.layoutMargins.left
+        blueprintsView.frame.origin.y = view.bounds.height - blueprintsView.bounds.height - 32
+    }
+}
+
+extension MapController: MapViewControllerDelegate {
+    func mapViewController(_: MapViewController, didSelect building: Building) {
+        if let blueprintViewController = blueprintsViewController {
+            blueprintViewController.building = building
+            return
+        }
+
+        let blueprintViewController = makeBlueprintsViewController(for: building)
+        addChild(blueprintViewController)
+
+        let blueprintsView: UIView = blueprintViewController.view
+        blueprintsView.backgroundColor = .fos_systemBackground
+        blueprintsView.alpha = 0
+        blueprintsView.layer.cornerRadius = 8
+        blueprintsView.layer.shadowRadius = 8
+        blueprintsView.layer.shadowOpacity = 0.2
+        blueprintsView.layer.shadowOffset = .zero
+        blueprintsView.layer.shadowColor = UIColor.black.cgColor
+        view.addSubview(blueprintsView)
+
+        let animator = UIViewPropertyAnimator(duration: 0.3, dampingRatio: 0.8)
+        animator.addAnimations { [weak self] in
+            guard let self = self else { return }
+            self.blueprintsViewController?.view.alpha = 1
+        }
+        animator.addCompletion { [weak self] _ in
+            guard let self = self else { return }
+            self.blueprintsViewController?.didMove(toParent: self)
+        }
+        animator.startAnimation()
     }
 
-    private func makeMapViewController() -> MapViewController {
+    func mapViewControllerDidDeselectBuilding(_: MapViewController) {
+        blueprintsViewController?.willMove(toParent: nil)
+
+        let animator = UIViewPropertyAnimator(duration: 0.3, dampingRatio: 0.8)
+        animator.addAnimations { [weak self] in
+            self?.blueprintsViewController?.view.alpha = 0
+        }
+        animator.addCompletion { [weak self] _ in
+            self?.blueprintsViewController?.view.removeFromSuperview()
+            self?.blueprintsViewController?.removeFromParent()
+        }
+        animator.startAnimation()
+    }
+}
+
+extension MapController: BlueprintViewControllerDelegate {
+    func blueprintViewControllerDidTapDismiss(_: BlueprintsViewController) {
+        mapViewController?.deselectSelectedAnnotation()
+    }
+}
+
+private extension MapController {
+    func makeMapViewController() -> MapViewController {
         let mapViewController = MapViewController()
         mapViewController.delegate = self
         self.mapViewController = mapViewController
         return mapViewController
     }
-}
 
-extension MapController: MapViewControllerDelegate {
-    func mapViewController(_ mapViewController: MapViewController, didSelect building: Building) {
-        print(#function, mapViewController, building)
+    func makeBlueprintsViewController(for building: Building) -> BlueprintsViewController {
+        let blueprintsViewController = BlueprintsViewController()
+        blueprintsViewController.building = building
+        blueprintsViewController.delegate = self
+        self.blueprintsViewController = blueprintsViewController
+        return blueprintsViewController
     }
 }
