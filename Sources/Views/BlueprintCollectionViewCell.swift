4555df568720fb35559e231476d1e65d12f45c38
mttcrsp <crespi.matteo@icloud.com>
2020-04-13 13:03:58 +0200

    Add blueprints dark mode support
    Closes #30

--- Sources/Views/BlueprintCollectionViewCell.swift
+++ Sources/Views/BlueprintCollectionViewCell.swift
@@ -1,3 +1,4 @@
+import CoreImage
 import UIKit
 
 final class BlueprintCollectionViewCell: UICollectionViewCell {
@@ -14,11 +15,42 @@ final class BlueprintCollectionViewCell: UICollectionViewCell {
     }
 
     func configure(with blueprint: Blueprint) {
-        imageView.image = UIImage(named: blueprint.imageName)
+        let blueprintImage = UIImage(named: blueprint.imageName)
+
+        if #available(iOS 12.0, *), traitCollection.userInterfaceStyle == .dark {
+            imageView.image = blueprintImage?.inverted
+        } else {
+            imageView.image = blueprintImage
+        }
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        if #available(iOS 12.0, *), traitCollection.userInterfaceStyle != previousTraitCollection?.userInterfaceStyle {
+            imageView.image = imageView.image?.inverted
+        }
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
         imageView.frame = contentView.bounds.inset(by: layoutMargins)
+    }
+}
+
+private extension UIImage {
+    var inverted: UIImage? {
+        guard let ciImage = CIImage(image: self) else { return nil }
+
+        let filter = CIFilter(name: "CIColorInvert")
+        filter?.setDefaults()
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+
+        let context = CIContext(options: nil)
+        if let output = filter?.outputImage, let copy = context.createCGImage(output, from: output.extent) {
+            return UIImage(cgImage: copy, scale: scale, orientation: .up)
+        } else {
+            return nil
+        }
     }
 }
