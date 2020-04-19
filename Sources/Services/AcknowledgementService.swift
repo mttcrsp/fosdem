02a2df6fa6c5b200e266bf48f7bef422213c39da
mttcrsp <crespi.matteo@icloud.com>
2020-04-19 18:21:39 +0200

    Add acknowledgements service

---
+++ Sources/Services/AcknowledgementService.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+protocol AcknowledgementsServiceBundle {
+    func url(forResource name: String?, withExtension ext: String?) -> URL?
+}
+
+protocol AcknowledgementsServiceDataProvider {
+    func data(withContentsOf url: URL) throws -> Data
+}
+
+final class AcknowledgementsService {
+    enum Error: CustomNSError {
+        case resourceNotFound
+    }
+
+    private let dataProvider: AcknowledgementsServiceDataProvider
+    private let bundle: AcknowledgementsServiceBundle
+
+    init(bundle: AcknowledgementsServiceBundle = Bundle.main, dataProvider: AcknowledgementsServiceDataProvider = AcknowledgementsServiceData()) {
+        self.dataProvider = dataProvider
+        self.bundle = bundle
+    }
+
+    func loadAcknowledgements() throws -> [Acknowledgement] {
+        guard let url = bundle.url(forResource: "acknowledgements", withExtension: "json") else {
+            throw Error.resourceNotFound
+        }
+
+        let data = try dataProvider.data(withContentsOf: url)
+        let decoder = JSONDecoder()
+        return try decoder.decode([Acknowledgement].self, from: data)
+    }
+}
+
+extension Bundle: AcknowledgementsServiceBundle {}
+
+final class AcknowledgementsServiceData: AcknowledgementsServiceDataProvider {
+    func data(withContentsOf url: URL) throws -> Data {
+        try Data(contentsOf: url)
+    }
+}
---
+++ Tests/AcknowledgementServiceTests.swift
@@ -0,0 +1,73 @@
+@testable
+import Fosdem
+import XCTest
+
+final class AcknowledgementsServiceTests: XCTestCase {
+    func testLoadAcknowledgements() {
+        XCTAssertNoThrow(try {
+            let data = Data("""
+            [
+                {"name":"a","url":"https://fosdem.org/a"},
+                {"name":"b","url":"https://fosdem.org/b"}
+            ]
+            """.utf8)
+            let dataProvider = AcknowledgementsServiceDataProviderMock(data: .success(data))
+            let bundleURL = URL(fileURLWithPath: "/fosdem")
+            let bundle = AcknowledgementsServiceBundleMock(url: bundleURL)
+            let service = AcknowledgementsService(bundle: bundle, dataProvider: dataProvider)
+
+            let acknowledgements1 = try service.loadAcknowledgements()
+            let acknowledgements2 = [
+                Acknowledgement(name: "a", url: URL(string: "https://fosdem.org/a")!),
+                Acknowledgement(name: "b", url: URL(string: "https://fosdem.org/b")!),
+            ]
+            XCTAssertEqual(acknowledgements1, acknowledgements2)
+        }())
+    }
+
+    func testLoadAcknowledgementsBundleError() {
+        do {
+            let data = Data()
+            let dataProvider = AcknowledgementsServiceDataProviderMock(data: .success(data))
+            let bundle = AcknowledgementsServiceBundleMock(url: nil)
+            let service = AcknowledgementsService(bundle: bundle, dataProvider: dataProvider)
+            _ = try service.loadAcknowledgements()
+            XCTFail("Unexpectedly succeeded in loading acknowledgements")
+        } catch {
+            let error1 = error as NSError
+            let error2 = AcknowledgementsService.Error.resourceNotFound as NSError
+            XCTAssertEqual(error1, error2)
+        }
+    }
+
+    func testLoadAcknowledgementsDataError() {
+        let error1 = NSError(domain: "test", code: 1)
+
+        do {
+            let dataProvider = AcknowledgementsServiceDataProviderMock(data: .failure(error1))
+            let bundleURL = URL(fileURLWithPath: "/fosdem")
+            let bundle = AcknowledgementsServiceBundleMock(url: bundleURL)
+            let service = AcknowledgementsService(bundle: bundle, dataProvider: dataProvider)
+            _ = try service.loadAcknowledgements()
+            XCTFail("Unexpectedly succeeded in loading acknowledgements")
+        } catch let error2 {
+            let error1 = error1 as NSError
+            let error2 = error2 as NSError
+            XCTAssertEqual(error1, error2)
+        }
+    }
+
+    func testloadAcknowledgementsDecodingError() {
+        do {
+            let data = Data()
+            let dataProvider = AcknowledgementsServiceDataProviderMock(data: .success(data))
+            let bundleURL = URL(fileURLWithPath: "/fosdem")
+            let bundle = AcknowledgementsServiceBundleMock(url: bundleURL)
+            let service = AcknowledgementsService(bundle: bundle, dataProvider: dataProvider)
+            _ = try service.loadAcknowledgements()
+            XCTFail("Unexpectedly succeeded in loading acknowledgements")
+        } catch {
+            XCTAssert(error is DecodingError)
+        }
+    }
+}
