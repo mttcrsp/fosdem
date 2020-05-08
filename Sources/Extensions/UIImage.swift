ae81bf941faf59a2cbe2ee203e7ddc3c06bf0287
mttcrsp <crespi.matteo@icloud.com>
2020-05-08 22:08:07 +0200

    Extract UIImage extension

---
+++ Sources/Extensions/UIImage.swift
@@ -0,0 +1,19 @@
+import CoreImage
+import UIKit
+
+extension UIImage {
+    var inverted: UIImage? {
+        guard let ciImage = CIImage(image: self) else { return nil }
+
+        let filter = CIFilter(name: "CIColorInvert")
+        filter?.setDefaults()
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+
+        let context = CIContext(options: nil)
+        if let output = filter?.outputImage, let copy = context.createCGImage(output, from: output.extent) {
+            return UIImage(cgImage: copy, scale: scale, orientation: .up)
+        } else {
+            return nil
+        }
+    }
+}
