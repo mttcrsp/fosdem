9add177df2b8c40d31da466b4d6901f0cec27d97
mttcrsp <crespi.matteo@icloud.com>
2020-05-10 00:48:03 +0200

    Add event metadata view

---
+++ Sources/Views/EventMetadataView.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+final class EventMetadataView: UIView {
+    var text: String? {
+        get { label.text }
+        set { label.text = newValue }
+    }
+
+    var image: UIImage? {
+        get { imageView.image }
+        set { imageView.image = newValue }
+    }
+
+    private let label = UILabel()
+    private let imageView = UIImageView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        label.numberOfLines = 0
+        label.font = .fos_preferredFont(forTextStyle: .subheadline)
+
+        for subview in [imageView, label] {
+            subview.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(subview)
+        }
+
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.centerYAnchor.constraint(equalTo: label.centerYAnchor),
+            imageView.trailingAnchor.constraint(equalTo: label.leadingAnchor, constant: -10),
+
+            label.topAnchor.constraint(equalTo: topAnchor),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
