f0367bcce7fba4960b0dabfab947f204ee963fed
mttcrsp <crespi.matteo@icloud.com>
2020-05-16 21:34:02 +0200

    Fix attachment icon layout issue

--- Sources/Views/EventAttachmentView.swift
+++ Sources/Views/EventAttachmentView.swift
@@ -13,6 +13,8 @@ final class EventAttachmentView: UIControl {
 
         label.numberOfLines = 0
         label.font = .fos_preferredFont(forTextStyle: .body, withSymbolicTraits: [.traitItalic])
+
+        imageView.contentMode = .center
         imageView.image = .fos_systemImage(withName: "arrow.down.circle")
 
         for subview in [imageView, label] {
@@ -26,6 +28,8 @@ final class EventAttachmentView: UIControl {
             label.leadingAnchor.constraint(equalTo: leadingAnchor),
             label.trailingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: -10),
 
+            imageView.widthAnchor.constraint(equalToConstant: 20),
+            imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor),
             imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
             imageView.centerYAnchor.constraint(equalTo: label.centerYAnchor),
         ])
