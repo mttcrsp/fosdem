7837d053871d81cbca2668e1fc13243f0d5b1591
mttcrsp <crespi.matteo@icloud.com>
2020-05-24 22:29:42 +0200

    Disable dynamic type on iOS 10

--- Sources/Extensions/UIFont+Extensions.swift
+++ Sources/Extensions/UIFont+Extensions.swift
@@ -2,13 +2,21 @@ import UIKit
 
 extension UIFont {
     class func fos_preferredFont(forTextStyle style: UIFont.TextStyle, withSymbolicTraits traits: UIFontDescriptor.SymbolicTraits = []) -> UIFont {
-        let descriptorOriginal = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
-        let descriptor = descriptorOriginal.withSymbolicTraits(traits) ?? descriptorOriginal
-        let font = UIFont(descriptor: descriptor, size: 0)
         if #available(iOS 11.0, *) {
+            let descriptorOriginal = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+            let descriptor = descriptorOriginal.withSymbolicTraits(traits) ?? descriptorOriginal
+            let font = UIFont(descriptor: descriptor, size: 0)
             return UIFontMetrics.default.scaledFont(for: font)
         } else {
-            return font
+            let traitCollection = UITraitCollection(preferredContentSizeCategory: .large)
+            let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style, compatibleWith: traitCollection)
+            if traits.contains(.traitBold) {
+                return UIFont.boldSystemFont(ofSize: descriptor.pointSize)
+            } else if traits.contains(.traitItalic) {
+                return UIFont.italicSystemFont(ofSize: descriptor.pointSize)
+            } else {
+                return UIFont.systemFont(ofSize: descriptor.pointSize)
+            }
         }
     }
 }
