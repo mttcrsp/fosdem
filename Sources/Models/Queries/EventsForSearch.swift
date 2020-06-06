2ddc269accb1c966676686e5cd94664b3fbaee10
mttcrsp <crespi.matteo@icloud.com>
2020-06-06 22:48:30 +0200

    Improve search query handling
    Closes #84

--- Sources/Models/Queries/EventsForSearch.swift
+++ Sources/Models/Queries/EventsForSearch.swift
@@ -4,11 +4,20 @@ struct EventsForSearch: PersistenceServiceRead {
     let query: String
 
     func perform(in database: Database) throws -> [Event] {
-        try Event.fetchAll(database, sql: """
+        let components = query.components(separatedBy: " ")
+        let componentsEscaped = components.map { "\"\($0)\"" }
+
+        let predicate = componentsEscaped.joined(separator: " OR ")
+        let predicateWithPrefix = "\(predicate) *"
+
+        let pattern = try database.makeFTS5Pattern(rawPattern: predicateWithPrefix, forTable: Event.searchDatabaseTableName)
+
+        return try Event.fetchAll(database, sql: """
         SELECT events.*
         FROM events JOIN events_search ON events.id = events_search.id
         WHERE events_search MATCH ?
         ORDER BY bm25(events_search, 5.0, 2.0, 5.0, 3.0, 1.0, 1.0, 3.0)
-        """, arguments: [FTS5Pattern(matchingAnyTokenIn: query)])
+        LIMIT 50
+        """, arguments: [pattern])
     }
 }
