77cd9314fc122c8a5cf5ce9c3a3330facdaf2a21
mttcrsp <crespi.matteo@icloud.com>
2020-06-21 00:23:39 +0200

    Move screenshots extraction as last operation

--- screenshots.sh
+++ screenshots.sh
@@ -1,11 +1,13 @@
-declare -a arr=("iPad (7th generation)" "iPad Pro (11-inch) (2nd generation)" "iPad Pro (12.9-inch) (4th generation)") # "iPhone 8" "iPhone 8 Plus" "iPhone 11 Pro" "iPhone 11 Pro Max"
+declare -a arr=("iPhone 8" "iPhone 8 Plus" "iPhone 11 Pro" "iPhone 11 Pro Max" "iPad (7th generation)" "iPad Pro (11-inch) (2nd generation)" "iPad Pro (12.9-inch) (4th generation)")
 
 for i in "${arr[@]}"
 do
   xcrun simctl boot "$i"
   xcrun simctl status_bar "$i" override --cellularBars 4 --time "9:41" --batteryLevel 100
   xcodebuild -scheme FOSDEM -sdk iphonesimulator -destination "name=$i" -only-testing UITests/ScreenshotTests/testScreenshots -derivedDataPath ./Build/ test | xcpretty
-  xcparse screenshots $(find build/Logs/Test/*.xcresult/ | head -n 1) screenshots/
+  mv $(find build/Logs/Test/*.xcresult/ | head -n 1) ./Screenshots
   rm -rf ./Build
   xcrun simctl shutdown "$i"
 done
+
+find ./Screenshots -name \*.xcresult -maxdepth 1 -exec xcparse screenshots {} ./Screenshots \;
