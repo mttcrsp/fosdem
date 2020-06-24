9489ed34d51a17d95d823ce4c9ae9eb5424424e6
mttcrsp <crespi.matteo@icloud.com>
2020-06-24 08:50:08 +0200

    Test playback service watch status and observers

--- Tests/PlaybackServiceTests.swift
+++ Tests/PlaybackServiceTests.swift
@@ -13,31 +13,28 @@ final class PlaybackServiceTests: XCTestCase {
   }
 
   func testSetPositionAt() {
-    let identifier = 1
     let position1 = PlaybackPosition.at(99)
 
     let userDefaults = PlaybackServiceDefaultsMock()
     let service = PlaybackService(userDefaults: userDefaults)
-    service.setPlaybackPosition(position1, forEventWithIdentifier: identifier)
+    service.setPlaybackPosition(position1, forEventWithIdentifier: .identifier1)
 
-    let position2 = service.playbackPosition(forEventWithIdentifier: identifier)
+    let position2 = service.playbackPosition(forEventWithIdentifier: .identifier1)
     XCTAssertEqual(position1, position2)
   }
 
   func testSetPositionEnd() {
-    let identifier = 1
     let position1 = PlaybackPosition.end
 
     let userDefaults = PlaybackServiceDefaultsMock()
     let service = PlaybackService(userDefaults: userDefaults)
-    service.setPlaybackPosition(position1, forEventWithIdentifier: identifier)
+    service.setPlaybackPosition(position1, forEventWithIdentifier: .identifier1)
 
-    let position2 = service.playbackPosition(forEventWithIdentifier: identifier)
+    let position2 = service.playbackPosition(forEventWithIdentifier: .identifier1)
     XCTAssertEqual(position1, position2)
   }
 
   func testSetPositionMixed() {
-    let identifier = 1
     let position1 = PlaybackPosition.at(50)
     let position2 = PlaybackPosition.end
     let position3 = PlaybackPosition.at(99)
@@ -45,24 +42,108 @@ final class PlaybackServiceTests: XCTestCase {
 
     let userDefaults = PlaybackServiceDefaultsMock()
     let service = PlaybackService(userDefaults: userDefaults)
-    service.setPlaybackPosition(position1, forEventWithIdentifier: identifier)
+    service.setPlaybackPosition(position1, forEventWithIdentifier: .identifier1)
 
-    let position5 = service.playbackPosition(forEventWithIdentifier: identifier)
+    let position5 = service.playbackPosition(forEventWithIdentifier: .identifier1)
     XCTAssertEqual(position1, position5)
 
-    service.setPlaybackPosition(position2, forEventWithIdentifier: identifier)
+    service.setPlaybackPosition(position2, forEventWithIdentifier: .identifier1)
 
-    let position6 = service.playbackPosition(forEventWithIdentifier: identifier)
+    let position6 = service.playbackPosition(forEventWithIdentifier: .identifier1)
     XCTAssertEqual(position2, position6)
 
-    service.setPlaybackPosition(position3, forEventWithIdentifier: identifier)
+    service.setPlaybackPosition(position3, forEventWithIdentifier: .identifier1)
 
-    let position7 = service.playbackPosition(forEventWithIdentifier: identifier)
+    let position7 = service.playbackPosition(forEventWithIdentifier: .identifier1)
     XCTAssertEqual(position3, position7)
 
-    service.setPlaybackPosition(position4, forEventWithIdentifier: identifier)
+    service.setPlaybackPosition(position4, forEventWithIdentifier: .identifier1)
 
-    let position8 = service.playbackPosition(forEventWithIdentifier: identifier)
+    let position8 = service.playbackPosition(forEventWithIdentifier: .identifier1)
     XCTAssertEqual(position4, position8)
   }
+
+  func testWatching() {
+    let userDefaults = PlaybackServiceDefaultsMock()
+    let service = PlaybackService(userDefaults: userDefaults)
+    service.setPlaybackPosition(.at(50), forEventWithIdentifier: .identifier1)
+    XCTAssertEqual(service.watching, [.identifier1])
+
+    service.setPlaybackPosition(.at(99), forEventWithIdentifier: .identifier2)
+    XCTAssertEqual(service.watching, [.identifier1, .identifier2])
+
+    service.setPlaybackPosition(.beginning, forEventWithIdentifier: .identifier2)
+    XCTAssertEqual(service.watching, [.identifier1])
+
+    service.setPlaybackPosition(.end, forEventWithIdentifier: .identifier1)
+    XCTAssertEqual(service.watching, [])
+  }
+
+  func testWatched() {
+    let userDefaults = PlaybackServiceDefaultsMock()
+    let service = PlaybackService(userDefaults: userDefaults)
+    service.setPlaybackPosition(.end, forEventWithIdentifier: .identifier1)
+    XCTAssertEqual(service.watched, [.identifier1])
+
+    service.setPlaybackPosition(.end, forEventWithIdentifier: .identifier2)
+    XCTAssertEqual(service.watched, [.identifier1, .identifier2])
+
+    service.setPlaybackPosition(.beginning, forEventWithIdentifier: .identifier2)
+    XCTAssertEqual(service.watched, [.identifier1])
+
+    service.setPlaybackPosition(.at(99), forEventWithIdentifier: .identifier2)
+    XCTAssertEqual(service.watched, [.identifier1])
+
+    service.setPlaybackPosition(.beginning, forEventWithIdentifier: .identifier1)
+    XCTAssertEqual(service.watched, [])
+  }
+
+  func testObservers() {
+    let userDefaults = PlaybackServiceDefaultsMock()
+    let service = PlaybackService(userDefaults: userDefaults)
+
+    var invocationsCount = 0
+    let observer = service.addObserver {
+      invocationsCount += 1
+    }
+
+    service.setPlaybackPosition(.beginning, forEventWithIdentifier: .identifier1)
+    XCTAssertEqual(invocationsCount, 0)
+
+    service.setPlaybackPosition(.beginning, forEventWithIdentifier: .identifier1)
+    XCTAssertEqual(invocationsCount, 0)
+
+    service.setPlaybackPosition(.at(50), forEventWithIdentifier: .identifier1)
+    XCTAssertEqual(invocationsCount, 1)
+
+    service.setPlaybackPosition(.at(50), forEventWithIdentifier: .identifier1)
+    XCTAssertEqual(invocationsCount, 1)
+
+    service.setPlaybackPosition(.at(51), forEventWithIdentifier: .identifier1)
+    XCTAssertEqual(invocationsCount, 1)
+
+    service.setPlaybackPosition(.beginning, forEventWithIdentifier: .identifier2)
+    XCTAssertEqual(invocationsCount, 1)
+
+    service.setPlaybackPosition(.at(99), forEventWithIdentifier: .identifier2)
+    XCTAssertEqual(invocationsCount, 2)
+
+    service.setPlaybackPosition(.end, forEventWithIdentifier: .identifier1)
+    XCTAssertEqual(invocationsCount, 3)
+
+    service.setPlaybackPosition(.at(100), forEventWithIdentifier: .identifier2)
+    XCTAssertEqual(invocationsCount, 3)
+
+    service.setPlaybackPosition(.end, forEventWithIdentifier: .identifier2)
+    XCTAssertEqual(invocationsCount, 4)
+
+    service.removeObserver(observer)
+    service.setPlaybackPosition(.at(50), forEventWithIdentifier: .identifier1)
+    XCTAssertEqual(invocationsCount, 4)
+  }
+}
+
+private extension Int {
+  static var identifier1: Int { 1 }
+  static var identifier2: Int { 2 }
 }
