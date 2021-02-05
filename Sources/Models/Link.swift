ba48dff2349bc99d71b0bd6552e3bec8c05a3f5d
mttcrsp <crespi.matteo@icloud.com>
2021-02-06 00:03:00 +0100

    Recognize livestream URLs

--- Sources/Models/Link.swift
+++ Sources/Models/Link.swift
@@ -3,3 +3,17 @@ import Foundation
 struct Link: Equatable, Codable {
   let name: String, url: URL?
 }
+
+extension Link {
+  var livestreamURL: URL? {
+    guard let host = url?.host, let pathComponents = url?.pathComponents else {
+      return nil
+    }
+
+    if host == "live.fosdem.org", pathComponents.contains("watch"), let identifier = pathComponents.last {
+      return URL(string: "https://stream.fosdem.org")?.appendingPathComponent(identifier).appendingPathExtension("m3u8")
+    } else {
+      return nil
+    }
+  }
+}
---
+++ Tests/LinkTests.swift
@@ -0,0 +1,15 @@
+@testable
+import Fosdem
+import XCTest
+
+final class LinkTests: XCTestCase {
+  func testLivestreamURL1() {
+    let link = Link(name: "test", url: URL(string: "https://live.fosdem.org/watch/mcommunity"))
+    XCTAssertEqual(link.livestreamURL, URL(string: "https://stream.fosdem.org/mcommunity.m3u8"))
+  }
+
+  func testLivestreamURL2() {
+    let link = Link(name: "test", url: URL(string: "https://live.fosdem.org/watch/dwebperformance"))
+    XCTAssertEqual(link.livestreamURL, URL(string: "https://stream.fosdem.org/dwebperformance.m3u8"))
+  }
+}
