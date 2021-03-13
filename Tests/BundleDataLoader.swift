a5dcf968b6d1bf509d620e5fd8da1b68b5ef1097
Matteo Crespi <crespi.matteo@icloud.com>
2021-03-13 15:30:34 +0100

    Adopt Tuist generated Bundle utility

--- Tests/BundleDataLoader.swift
+++ Tests/BundleDataLoader.swift
@@ -4,18 +4,12 @@ import XCTest
 final class BundleDataLoader {
   private let bundle: Bundle
 
-  init(bundle: Bundle = .default) {
+  init(bundle: Bundle = .module) {
     self.bundle = bundle
   }
 
   func data(forResource resource: String, withExtension ext: String) throws -> Data {
     let url = try XCTUnwrap(bundle.url(forResource: resource, withExtension: ext))
     return try Data(contentsOf: url)
-  }
-}
-
-private extension Bundle {
-  static var `default`: Bundle {
-    .init(for: BundleDataLoader.self)
   }
 }
