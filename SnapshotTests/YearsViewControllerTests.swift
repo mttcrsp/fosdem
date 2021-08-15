a9106949ed07131d10eb8fc144db0d2eb67e8d8e
Matteo Crespi <crespi.matteo@icloud.com>
2021-08-16 00:54:38 +0200

    Update years view controller tests

--- SnapshotTests/YearsViewControllerTests.swift
+++ SnapshotTests/YearsViewControllerTests.swift
@@ -4,22 +4,40 @@ import SnapshotTesting
 import XCTest
 
 final class YearsViewControllerTests: XCTestCase {
-  private let dataSource: YearsViewControllerDataSourceMock = {
+  private func makeDataSource(with states: [YearDownloadState] = YearDownloadState.allCases) -> YearsViewControllerDataSourceMock {
     let dataSource = YearsViewControllerDataSourceMock()
+    dataSource.numberOfYearsHandler = { _ in states.count }
     dataSource.yearsViewControllerHandler = { _, index in index }
-    dataSource.yearsViewControllerDownloadStateAtHandler = { _, index in YearDownloadState.allCases[index] }
-    dataSource.numberOfYearsHandler = { _ in YearDownloadState.allCases.count }
+    dataSource.yearsViewControllerDownloadStateAtHandler = { _, index in states[index] }
     return dataSource
-  }()
+  }
 
-  func testAppearance() throws {
+  func testAppearance() {
+    let dataSource1 = makeDataSource(with: [.available, .inProgress, .completed])
+    let dataSource2 = makeDataSource(with: [.inProgress, .completed, .inProgress])
+
     let yearsViewController = YearsViewController()
-    yearsViewController.dataSource = dataSource
+    yearsViewController.dataSource = dataSource1
     yearsViewController.view.tintColor = .fos_label
     assertSnapshot(matching: yearsViewController, as: .image(on: .iPhone8Plus))
+
+    yearsViewController.dataSource = dataSource2
+    yearsViewController.reloadDownloadState(at: 0)
+    yearsViewController.reloadDownloadState(at: 1)
+    yearsViewController.reloadDownloadState(at: 2)
+    assertSnapshot(matching: yearsViewController, as: .image(on: .iPhone8Plus))
+
+    if #available(iOS 13.0, *) {
+      yearsViewController.overrideUserInterfaceStyle = .dark
+      yearsViewController.reloadDownloadState(at: 0)
+      yearsViewController.reloadDownloadState(at: 1)
+      yearsViewController.reloadDownloadState(at: 2)
+      assertSnapshot(matching: yearsViewController, as: .image(on: .iPhone8Plus))
+    }
   }
 
   func testEvents() throws {
+    let dataSource = makeDataSource()
     let delegate = YearsViewControllerDelegateMock()
 
     let yearsViewController = YearsViewController()
@@ -33,5 +51,21 @@ final class YearsViewControllerTests: XCTestCase {
     XCTAssertEqual(delegate.yearsViewControllerCallCount, 1)
     XCTAssertEqual(delegate.yearsViewControllerArgValues.first?.0, yearsViewController)
     XCTAssertEqual(delegate.yearsViewControllerArgValues.first?.1, 1)
+  }
+
+  func testAllowsSelection() throws {
+    let yearsViewController = YearsViewController()
+
+    let tableView = yearsViewController.view.findSubview(ofType: UITableView.self)
+    XCTAssertEqual(tableView?.allowsSelection, true)
+    XCTAssertEqual(yearsViewController.allowsSelection, true)
+
+    yearsViewController.allowsSelection = false
+    XCTAssertEqual(tableView?.allowsSelection, false)
+    XCTAssertEqual(yearsViewController.allowsSelection, false)
+
+    yearsViewController.allowsSelection = true
+    XCTAssertEqual(tableView?.allowsSelection, true)
+    XCTAssertEqual(yearsViewController.allowsSelection, true)
   }
 }
