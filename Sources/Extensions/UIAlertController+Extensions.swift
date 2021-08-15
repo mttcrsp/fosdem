940ef79063a2e5b7d5e7f0425ebf699e4102cdd4
Matteo Crespi <crespi.matteo@icloud.com>
2021-08-16 00:18:56 +0200

    No internet connection alert

--- Sources/Extensions/UIAlertController+Extensions.swift
+++ Sources/Extensions/UIAlertController+Extensions.swift
@@ -2,14 +2,25 @@ import UIKit
 
 extension UIAlertController {
   static func makeErrorController(withHandler handler: (() -> Void)? = nil) -> UIAlertController {
-    let title = L10n.Error.Alert.title
-    let message = L10n.Error.Alert.message
-    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-
     let dismissTitle = L10n.Error.dismiss
     let dismissAction = UIAlertAction(title: dismissTitle, style: .default) { _ in handler?() }
-    alertController.addAction(dismissAction)
 
+    let title = L10n.Error.Unknown.title, message = L10n.Error.Unknown.message
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    alertController.addAction(dismissAction)
+    return alertController
+  }
+
+  static func makeNoInternetController(withRetryHandler handler: @escaping () -> Void) -> UIAlertController {
+    let dismissTitle = L10n.Error.dismiss
+    let dismissAction = UIAlertAction(title: dismissTitle, style: .cancel)
+
+    let retryTitle = L10n.Error.retry
+    let retryAction = UIAlertAction(title: retryTitle, style: .default) { _ in handler() }
+
+    let title = L10n.Error.Internet.title, message = L10n.Error.Internet.message
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    alertController.addAction(dismissAction)
     return alertController
   }
 }
