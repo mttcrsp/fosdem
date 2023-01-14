d66fad7e2c4a2f0fb9af5d69f43d923292494e7b
Matteo Crespi <crespi.matteo@icloud.com>
2023-01-14 11:12:35 +0100

    Update for GRDB breaking change

--- Sources/Models/Queries/AllTracks.swift
+++ Sources/Models/Queries/AllTracks.swift
@@ -3,7 +3,7 @@ import GRDB
 struct AllTracksOrderedByName: PersistenceServiceRead {
   func perform(in database: Database) throws -> [Track] {
     try Track
-      .filter(Track.Columns.name.like("% stand").negated)
+      .filter(!Track.Columns.name.like("% stand"))
       .order(Track.Columns.name.lowercased)
       .fetchAll(database)
   }
