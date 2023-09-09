1cb208d0a24bf0804dcaaff78bf876afbe1f64c3
Matteo Crespi <crespi.matteo@icloud.com>
2023-09-09 14:17:38 +0200

    Migrate update service

--- App/Sources/Services/UpdateService.swift
+++ App/Sources/Services/UpdateService.swift
@@ -1,41 +1,39 @@
 import Foundation
 
-final class UpdateService {
-  private let bundle: UpdateServiceBundle
-  private let networkService: UpdateServiceNetwork
+struct UpdateService {
+  var detectUpdates: (@escaping () -> Void) -> Void
+}
 
+extension UpdateService {
   init(networkService: UpdateServiceNetwork, bundle: UpdateServiceBundle = Bundle.main) {
-    self.networkService = networkService
-    self.bundle = bundle
-  }
-
-  func detectUpdates(completion: @escaping () -> Void) {
-    guard let bundleIdentifier = bundle.bundleIdentifier else {
-      return assertionFailure("Failed to acquire bundle identifier from bundle \(bundle)")
-    }
-
-    guard let bundleShortVersion = bundle.bundleShortVersion else {
-      return assertionFailure("Failed to acquire short bundle version from bundle \(bundle)")
-    }
-
-    let request = AppStoreSearchRequest()
-    networkService.perform(request) { result in
-      guard case let .success(response) = result else { return }
-
-      guard let result = response.results.first(where: { result in result.bundleIdentifier == bundleIdentifier }) else {
-        return assertionFailure("AppStore search request did not return any application with identifier \(bundleIdentifier)")
+    detectUpdates = { completion in
+      guard let bundleIdentifier = bundle.bundleIdentifier else {
+        return assertionFailure("Failed to acquire bundle identifier from bundle \(bundle)")
       }
 
-      if result.version.compare(bundleShortVersion, options: .numeric) == .orderedDescending {
-        completion()
+      guard let bundleShortVersion = bundle.bundleShortVersion else {
+        return assertionFailure("Failed to acquire short bundle version from bundle \(bundle)")
+      }
+
+      let request = AppStoreSearchRequest()
+      networkService.perform(request) { result in
+        guard case let .success(response) = result else { return }
+
+        guard let result = response.results.first(where: { result in result.bundleIdentifier == bundleIdentifier }) else {
+          return assertionFailure("AppStore search request did not return any application with identifier \(bundleIdentifier)")
+        }
+
+        if result.version.compare(bundleShortVersion, options: .numeric) == .orderedDescending {
+          completion()
+        }
       }
     }
   }
 }
 
 /// @mockable
-protocol UpdateServiceProtocol: AnyObject {
-  func detectUpdates(completion: @escaping () -> Void)
+protocol UpdateServiceProtocol {
+  var detectUpdates: (@escaping () -> Void) -> Void { get }
 }
 
 extension UpdateService: UpdateServiceProtocol {}
