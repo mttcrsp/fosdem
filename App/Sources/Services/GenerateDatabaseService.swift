b87897779b888aff5cfa44f2654e61ca28554094
Matteo Crespi <crespi.matteo@icloud.com>
2023-09-09 14:50:43 +0200

    Migrate generate database service

--- App/Sources/Services/GenerateDatabaseService.swift
+++ App/Sources/Services/GenerateDatabaseService.swift
@@ -1,31 +1,29 @@
 #if DEBUG
 import Foundation
 
-final class GenerateDatabaseService {
-  private let fileManager: FileManager
-  private let networkService: NetworkService
+struct GenerateDatabaseService {
+  var generate: (@escaping (Result<URL, Error>) -> Void) -> Void
+}
 
+extension GenerateDatabaseService {
   init(networkService: NetworkService = .init(session: URLSession.shared), fileManager: FileManager = .default) {
-    self.fileManager = fileManager
-    self.networkService = networkService
-  }
+    generate = { completion in
+      let databaseFile = fileManager.temporaryDirectory
+        .appendingPathComponent("db")
+        .appendingPathExtension("sqlite")
 
-  func generate(completion: @escaping (Result<URL, Error>) -> Void) {
-    let databaseFile = fileManager.temporaryDirectory
-      .appendingPathComponent("db")
-      .appendingPathExtension("sqlite")
-
-    networkService.perform(ScheduleRequest(year: YearsService.current)) { result in
-      switch result {
-      case let .failure(error):
-        completion(.failure(error))
-      case let .success(schedule):
-        do {
-          let persistenceService = try PersistenceService(path: databaseFile.path, migrations: .allMigrations)
-          try persistenceService.performWriteSync(UpsertSchedule(schedule: schedule))
-          completion(.success(databaseFile))
-        } catch {
+      networkService.perform(ScheduleRequest(year: YearsService.current)) { result in
+        switch result {
+        case let .failure(error):
           completion(.failure(error))
+        case let .success(schedule):
+          do {
+            let persistenceService = try PersistenceService(path: databaseFile.path, migrations: .allMigrations)
+            try persistenceService.performWriteSync(UpsertSchedule(schedule: schedule))
+            completion(.success(databaseFile))
+          } catch {
+            completion(.failure(error))
+          }
         }
       }
     }
